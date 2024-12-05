619cd443a03426d98821061d963ce5ddb0736f1e
Matt Dixon <dxn000@gmail.com>
2024-12-05 09:42:49 -0500

    Add GitHub Actions workflow for testing and publishing Python package

--- setup.py
+++ setup.py
@@ -2,18 +2,23 @@ from setuptools import setup, find_packages
 
 setup(
     name='vapos',
-    version='0.1.0',
-    author='Matthew Dixon',
+    version='0.1.0',  # update this for each new release
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    install_requires=[
+        'numpy',
+        'scipy',
+        'matplotlib',
+    ],
+    author='Mathew Dixon',
     author_email='dxn000@gmail.com',
     description='Vibrational and Power Optimizing Software',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    url='https://github.com/dxns-hub/vapos',
-    packages=find_packages(),
+    url='https://github.com/yourusername/vapos',
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
-    python_requires='>=3.6',
 )
